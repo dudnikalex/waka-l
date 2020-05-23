9a9f4d812e4c248e07df50eb45101f16600b013c
dudnik.aa <dudnik.aa@phystech.edu>
2020-05-24 00:47:53 +0300

    refactoring

--- app/routes.py
+++ app/routes.py
@@ -21,37 +21,21 @@ def svd_compress(filename, rank):
     image = np.array(Image.open(filename))
     image = image / 255
     row, col, _ = image.shape
-    image_red = image[:, :, 0]
-    image_green = image[:, :, 1]
-    image_blue = image[:, :, 2]
-
-    U_r, d_r, V_r = np.linalg.svd(image_red, full_matrices=True)
-    U_g, d_g, V_g = np.linalg.svd(image_green, full_matrices=True)
-    U_b, d_b, V_b = np.linalg.svd(image_blue, full_matrices=True)
-
-    U_r_k = U_r[:, 0:rank]
-    V_r_k = V_r[0:rank, :]
-    U_g_k = U_g[:, 0:rank]
-    V_g_k = V_g[0:rank, :]
-    U_b_k = U_b[:, 0:rank]
-    V_b_k = V_b[0:rank, :]
-
-    d_r_k = d_r[0:rank]
-    d_g_k = d_g[0:rank]
-    d_b_k = d_b[0:rank]
-
-    image_red_approx = np.dot(U_r_k, np.dot(np.diag(d_r_k), V_r_k))
-    image_green_approx = np.dot(U_g_k, np.dot(np.diag(d_g_k), V_g_k))
-    image_blue_approx = np.dot(U_b_k, np.dot(np.diag(d_b_k), V_b_k))
-
     new_image = np.zeros((row, col, 3))
 
-    new_image[:, :, 0] = image_red_approx
-    new_image[:, :, 1] = image_green_approx
-    new_image[:, :, 2] = image_blue_approx
+    for i in range(3):
+        layer = image[:, :, i]
 
-    new_image[new_image < 0] = 0
-    new_image[new_image > 1] = 1
+        U, d, V = np.linalg.svd(layer, full_matrices=True)
+
+        U_new = U[:, 0:rank]
+        V_new = V[0:rank, :]
+        d_new = d[0:rank]
+
+        layer = np.dot(U_new, np.dot(np.diag(d_new), V_new))
+        new_image[:, :, i] = layer
+
+    new_image = np.clip(new_image, 0, 1)
 
     new_image = (new_image * 255).astype(np.uint8)
     im = Image.fromarray(new_image)
