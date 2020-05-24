9984e3003c69fcace60dd2a3f001970b94e62d10
dudnik.aa <dudnik.aa@phystech.edu>
2020-05-25 00:31:22 +0300

    refactoring and adding resize function

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import IntegerField
 from flask_wtf.file import FileField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, NumberRange
 
 
-class ImageForm(FlaskForm):
+class SvdForm(FlaskForm):
     image = FileField('Image', validators=[DataRequired("Please, select an image")])
-    rank = IntegerField('Rank', validators=[DataRequired("Please, enter valid rank")])
+    rank = IntegerField('Rank', validators=[DataRequired("Please, enter valid rank"),
+                                            NumberRange(1, None, "Please, input postive value")])
+
+
+class ResizeForm(FlaskForm):
+    image = FileField('Image', validators=[DataRequired("Please, select an image")])
+    coefficient = IntegerField('Coefficient', validators=[DataRequired("Please, enter valid coefficient"),
+                                                          NumberRange(1, None, "Please, input postive value")])
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 from flask import render_template
 from app import app
-from app.forms import ImageForm
+from app.forms import SvdForm, ResizeForm
 import numpy as np
 from PIL import Image
 
 
 @app.route('/', methods=["GET", "POST"])
 def index():
-    image_form = ImageForm()
+    image_form = SvdForm()
     if image_form.validate_on_submit():
         image = image_form.image.data
         rank = image_form.rank.data
@@ -17,14 +17,24 @@ def index():
     return render_template('index.html', image_form=image_form)
 
 
+@app.route('/resize', methods=["GET", "POST"])
+def resize():
+    image_form = ResizeForm()
+    if image_form.validate_on_submit():
+        image = image_form.image.data
+        coefficient = image_form.coefficient.data
+        image.save(image.filename)
+        return render_template('result.html', filename=resize_compress(image.filename, coefficient))
+    return render_template('resize.html', image_form=image_form)
+
+
 def svd_compress(filename, rank):
     image = np.array(Image.open(filename))
     image = image / 255
-    row, col, _ = image.shape
-    new_image = np.zeros((row, col, 3))
+    compressed_image = np.zeros_like(image)
 
-    for i in range(3):
-        layer = image[:, :, i]
+    for depth in range(image.shape[-1]):
+        layer = image[:, :, depth]
 
         U, d, V = np.linalg.svd(layer, full_matrices=True)
 
@@ -33,11 +43,22 @@ def svd_compress(filename, rank):
         d_new = d[0:rank]
 
         layer = np.dot(U_new, np.dot(np.diag(d_new), V_new))
-        new_image[:, :, i] = layer
+        compressed_image[:, :, depth] = layer
 
-    new_image = np.clip(new_image, 0, 1)
+    compressed_image = np.clip(compressed_image, 0, 1)
 
-    new_image = (new_image * 255).astype(np.uint8)
-    im = Image.fromarray(new_image)
-    im.save("app/static/compressed_{}_{}".format(rank, filename))
-    return "static/compressed_{}_{}".format(rank, filename)
+    compressed_image = (compressed_image * 255).astype(np.uint8)
+    compressed_image = Image.fromarray(compressed_image)
+
+    compressed_image_path = "static/compressed_{}_{}".format(rank, filename)
+    compressed_image.save("app/" + compressed_image_path)
+    return compressed_image_path
+
+
+def resize_compress(filename, coefficient):
+    image = Image.open(filename)
+    image = image.resize(size=(image.size[0] // coefficient, image.size[1] // coefficient))
+
+    compressed_image_path = "static/resized_{}_{}".format(coefficient, filename)
+    image.save("app/" + compressed_image_path)
+    return compressed_image_path
